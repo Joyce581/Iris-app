dfb88d2c26665f6d01dd1316adac5ed43f78ef81
Joyce581 <joyceyamdjeu05@gmail.com>
2024-11-19 14:06:30 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -14,7 +14,8 @@ import altair as alt
 import pandas as pd
 
 # Create a chart
-chart = alt.Chart(data).mark_line().encode( x='Sepal Length', y='Petal Width') 
+chart = alt.Chart(data).mark_line().encode( x='SepalLength', y='PetalWidth') 
 
 # Afficher le chart sur Streamlit
 st.altair_chart(chart, use_container_width=True)
+
