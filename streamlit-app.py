290ddc5b9ad4d4f0017ad2c4711f6e78ae023d57
Joyce581 <joyceyamdjeu05@gmail.com>
2024-11-19 13:45:30 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -8,5 +8,13 @@ data = pd.read_csv('Iris.csv', delimiter=';')
 st.title("Mon premier tableau de bord Streamlit")
 
 # Afficher les données dans un tableau
-st.map(data)
+st.table(data)
 
+import altair as alt
+import pandas as pd
+
+# Create a chart
+chart = alt.Chart(data).mark_line(point=True).encode(
+      x = 'Sepal Length'
+      y = 'Petal Width'
+)
