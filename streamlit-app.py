6a086eed27819fa0d04ebb74de9f6a619c9cf256
Joyce581 <joyceyamdjeu05@gmail.com>
2024-11-19 16:37:18 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -22,29 +22,14 @@ st.altair_chart(chart, use_container_width=True)
 import streamlit as st
 
 # Add a title to the sidebar
-st.sidebar.title("Joyce's Sidebar Menu") 
+st.sidebar.title("Iris Visualisation Menu") 
 
 # Add widgets to the sidebar
 option = st.sidebar.selectbox("Choose an Option:", 
-              ["Data Classification", "Iris Classification"]
+              ["vertosa", "virginica", "versicolor"]
 )
 
-import streamlit as st
-import matplotlib.pyplot as plt
 
-# Les données pour le graphique
-labels = ['Setosa', 'Virginica', 'Versicolor']
-sizes = [15, 30, 45]  # Pourcentages des parts
-colors = ['gold', 'yellowgreen', 'lightcoral']
-explode = (0.1, 0, 0) # Exploser la première part (Catégorie A)
-
-# Création du graphique
-fig, ax = plt.subplots()
-ax.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
-ax.axis('equal')  # Assure que le diagramme est dessiné en cercle.
-
-# Affichage du graphique dans Streamlit
-st.pyplot(fig)
 
 
 
